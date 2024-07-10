8d07a7bf7d85abaacfa6bd40a598a004b470c1b1
mlgrzt <goskolarska@gmail.com>
2024-07-10 20:23:15 +0200

    added animation to faq

--- src/components/06 FAQ/FAQItem.js
+++ src/components/06 FAQ/FAQItem.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styles from "./FAQItem.module.css";
-
 import { ReactComponent as IconChevron } from "../../assets/images/icon-chevron.svg";
 
 function FAQItem({ question, answer }) {
   const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      contentRef.current.style.maxHeight = `${contentRef.current.scrollHeight}px`;
+    } else {
+      contentRef.current.style.maxHeight = "0px";
+    }
+  }, [isOpen]);
 
   return (
     <div className={styles.faqItem}>
@@ -14,12 +22,12 @@ function FAQItem({ question, answer }) {
           className={`${styles.icon} ${isOpen ? styles.iconOpen : ""}`}
         />
       </div>
-      {isOpen && (
+      <div className={styles.answerWrapper} ref={contentRef}>
         <div
           className={styles.answer}
           dangerouslySetInnerHTML={{ __html: answer }}
         />
-      )}
+      </div>
     </div>
   );
 }
