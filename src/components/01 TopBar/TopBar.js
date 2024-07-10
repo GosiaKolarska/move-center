e4837d09005eafaacb7ebc56c15a606dcc989b5f
mlgrzt <goskolarska@gmail.com>
2024-07-10 10:54:03 +0200

    top bar fixed desktop

--- src/components/01 TopBar/TopBar.js
+++ src/components/01 TopBar/TopBar.js
@@ -11,36 +11,41 @@ const defaultPhone = "48 667 745 056";
 
 function TopBar() {
   return (
-    <div className={styles.topBar}>
-      <div className="container flex">
-        <a href="/" className={styles.topBarLogo}>
-          <Logo className={styles.topBarLogoImage} />
-        </a>
-        <div className={styles.topBarContactInfo}>
-          <div className={styles.topBarContactInfoWrapper}>
-            <div className={styles.topBarProfileImageWrapper}>
-              <img
-                src={defaultProfileImageSrc}
-                alt={defaultTrainerName}
-                className={styles.topBarProfileImage}
-              />
-            </div>
-            <div className={styles.topBarTextInfo}>
-              <p className={styles.topBarName}>{defaultTrainerName}</p>
-              <p className={styles.topBarSubtitle}>{defaultTrainerSubtitle}</p>
-            </div>
-          </div>
-          <div className={styles.topBarContactDetails}>
-            <p className={styles.topBarPhone}>+{defaultPhone}</p>
-            <p className={styles.topBarContact}>Kontakt</p>
-          </div>
-          <a
-            href="#form"
-            className={styles.topBarConsultButton}
-            onClick={smoothScroll}
-          >
-            Zapisz się na <b>darmową</b> konsultację
+    <div className={styles.topBarWrapper}>
+      <div className={styles.topBarBackground}></div>
+      <div className={styles.topBar}>
+        <div className="container flex">
+          <a href="/" className={styles.topBarLogo}>
+            <Logo className={styles.topBarLogoImage} />
           </a>
+          <div className={styles.topBarContactInfo}>
+            <div className={styles.topBarContactInfoWrapper}>
+              <div className={styles.topBarProfileImageWrapper}>
+                <img
+                  src={defaultProfileImageSrc}
+                  alt={defaultTrainerName}
+                  className={styles.topBarProfileImage}
+                />
+              </div>
+              <div className={styles.topBarTextInfo}>
+                <p className={styles.topBarName}>{defaultTrainerName}</p>
+                <p className={styles.topBarSubtitle}>
+                  {defaultTrainerSubtitle}
+                </p>
+              </div>
+            </div>
+            <div className={styles.topBarContactDetails}>
+              <p className={styles.topBarPhone}>+{defaultPhone}</p>
+              <p className={styles.topBarContact}>Kontakt</p>
+            </div>
+            <a
+              href="#form"
+              className={styles.topBarConsultButton}
+              onClick={smoothScroll}
+            >
+              Zapisz się na <b>darmową</b> konsultację
+            </a>
+          </div>
         </div>
       </div>
     </div>
